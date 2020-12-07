4c1cdb9606df4fc4913a6851a99a90db59ba82fd
slimkrazy <sam@slimkrazy.com>
2020-12-07 10:53:04 +0000

    WIP

--- modules/verizonMediaBidAdapter.js
+++ modules/verizonMediaBidAdapter.js
@@ -18,16 +18,19 @@ const BIDDING_SOURCE = {
   version: '$prebid.version$'
 };
 */
-const SSP_ENDPOINT = 'https://c2shb.ssp.yahoo.com';
+const SSP_ENDPOINT = 'https://c2shb.ssp.yahoo.com/bidRequest?';
 
 /* Utility functions */
 function hasPurpose1Consent(bidderRequest) {
+  return false;
+  /*
   if (bidderRequest && bidderRequest.gdprConsent) {
     if (bidderRequest.gdprConsent.gdprApplies && bidderRequest.gdprConsent.apiVersion === 2) {
       return !!(utils.deepAccess(bidderRequest.gdprConsent, 'vendorData.purpose.consents.1') === true);
     }
   }
   return true;
+  */
 }
 
 function getSize(size) {
@@ -102,8 +105,8 @@ function generatePayload(bid, bidderRequest) {
 
 export const spec = {
   code: BIDDER_CODE,
-
   aliases: [],
+  supportedMediaTypes: [BANNER],
 
   isBidRequestValid: function(bid) {
     const params = bid.params;
@@ -116,14 +119,13 @@ export const spec = {
     let requestOptions = {
       contentType: 'application/json',
       customHeaders: {
-        'x-openrtb-version': '2.3'
+        // 'x-openrtb-version': '2.3'
       }
     };
 
     requestOptions.withCredentials = hasPurpose1Consent(bidderRequest);
-
     return validBidRequests.filter(bid => {
-      return bid.mediaType === BANNER;
+      return Object.keys(bid.mediaTypes).includes(BANNER);
     }).map(bid => {
       return {
         url: SSP_ENDPOINT,
